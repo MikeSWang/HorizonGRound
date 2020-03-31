Bug fixed: uncertainty to variance. Added Poisson.

horizonground/clustering_modification.py
@@ -144,8 +144,6 @@ corrections are
 |
 
 """
-from __future__ import division
-
 import numpy as np
 from nbodykit.lab import cosmology as nbk_cosmology
 
@@ -186,11 +184,11 @@ def standard_kaiser_factor(order, bias, redshift, cosmo=FIDUCIAL_COSMOLOGY):
     f = cosmo.scale_independent_growth_rate(redshift)
 
     if order == 0:
-        factor = b_1 ** 2 + 2/3 * f * b_1 + 1/5 * f**2
+        factor = b_1 ** 2 + 2./3. * f * b_1 + 1./5. * f**2
     elif order == 2:
-        factor = 4/3 * f * b_1 + 4/7 * f**2
+        factor = 4./3. * f * b_1 + 4./7. * f**2
     elif order == 4:
-        factor = 8/35 * f**2
+        factor = 8./35. * f**2
     else:
         factor = 0.
 
@@ -218,7 +216,7 @@ def scale_dependence_kernel(redshift, cosmo=FIDUCIAL_COSMOLOGY):
     """
     numerical_constants = 3 * _SPHERICAL_COLLAPSE_CRITICAL_OVERDENSITY \
         * cosmo.Om0 * _TRANSFER_FUNCTION_NORMALISATION \
-        * (100*FIDUCIAL_COSMOLOGY.h / _SPEED_OF_LIGHT_IN_KM_PER_S)**2 \
+        * (100 * FIDUCIAL_COSMOLOGY.h / _SPEED_OF_LIGHT_IN_KM_PER_S)**2 \
 
     transfer_function = nbk_cosmology.power.transfers.CLASS(cosmo, redshift)
 
@@ -263,9 +261,9 @@ def non_gaussianity_factor(wavenumber, order, local_NG, bias, redshift,
         / wavenumber**2
 
     if order == 0:
-        factor = b_1**2 + (2*b_1 + 2/3*f) * delta_b + delta_b**2
+        factor = b_1 ** 2 + (2 * b_1 + 2./3. * f) * delta_b + delta_b ** 2
     elif order == 2:
-        factor = 4/3 * f * delta_b
+        factor = 4./3. * f * delta_b
     else:
         factor = 0.
 
@@ -303,7 +301,7 @@ def relativistic_correction_func(cosmo=FIDUCIAL_COSMOLOGY, geometric=True,
 
     if geometric:
         geometric_term = lambda z: \
-            2 / chi(z) + aH(z) * (1 - 3/2 * astropy_cosmo.Om0 / a(z)**3)
+            2 / chi(z) + aH(z) * (1 - 3./2. * astropy_cosmo.Om0 / a(z) ** 3)
     else:
         geometric_term = lambda z: 0.
 
@@ -316,7 +314,7 @@ def relativistic_correction_func(cosmo=FIDUCIAL_COSMOLOGY, geometric=True,
         lensing_term = lambda z: 0.
     else:
         lensing_term = lambda z: \
-            5 * magnification_bias(z) * (aH(z) - 1/chi(z))
+            5 * magnification_bias(z) * (aH(z) - 1 / chi(z))
 
     return np.vectorize(
         lambda z: (geometric_term(z) + evolution_term(z) + lensing_term(z))
@@ -355,7 +353,8 @@ def relativistic_correction_eval(redshift, cosmo=FIDUCIAL_COSMOLOGY,
 
     correction_value = 0.
     if geometric:
-        correction_value += 2 / chi + aH * (1 - 3/2 * astropy_cosmo.Om0 / a**3)
+        correction_value += \
+            2 / chi + aH * (1 - 3./2. * astropy_cosmo.Om0 / a ** 3)
     if evolution_bias:
         correction_value += - aH * evolution_bias
     if magnification_bias:
@@ -401,9 +400,9 @@ def relativistic_factor(wavenumber, order, redshift, correction_value=None,
 
     """
     if correction_value is not None:
-        modification = correction_value**2 \
-            * cosmo.scale_independent_growth_rate(redshift)**2 \
-            / wavenumber**2
+        modification = correction_value ** 2 \
+            * cosmo.scale_independent_growth_rate(redshift) ** 2 \
+            / wavenumber ** 2
     elif callable(evolution_bias) and callable(magnification_bias):
         correction_function = relativistic_correction_func(
             cosmo=cosmo,
@@ -411,8 +410,8 @@ def relativistic_factor(wavenumber, order, redshift, correction_value=None,
             evolution_bias=evolution_bias,
             magnification_bias=magnification_bias
         )
-        modification = correction_function(redshift)**2 \
-            * cosmo.scale_independent_growth_rate(redshift)**2 \
+        modification = correction_function(redshift) ** 2 \
+            * cosmo.scale_independent_growth_rate(redshift) ** 2 \
             / wavenumber**2
     elif isinstance(evolution_bias, float) \
             and isinstance(magnification_bias, float):
@@ -423,19 +422,19 @@ def relativistic_factor(wavenumber, order, redshift, correction_value=None,
             evolution_bias=evolution_bias,
             magnification_bias=magnification_bias
         )
-        modification = correction_value**2 \
-            * cosmo.scale_independent_growth_rate(redshift)**2 \
-            / wavenumber**2
+        modification = correction_value ** 2 \
+            * cosmo.scale_independent_growth_rate(redshift) ** 2 \
+            / wavenumber ** 2
     else:
         raise TypeError(
             "`evolution_bias` and `magnification_bias` must be "
-            "both callable or float or None. "
+            "both callable or float or None."
         )
 
     if order == 0:
-        factor = 1/3 * modification
+        factor = 1./3. * modification
     elif order == 2:
-        factor = 2/3 * modification
+        factor = 2./3. * modification
     else:
         factor = 0.
 

horizonground/lumfunc_likelihood.py
@@ -24,26 +24,27 @@ Likelihood evaluation
 |
 
 """
-from __future__ import division
-
 import warnings
 from collections import OrderedDict
 from itertools import compress as iterfilter
 from itertools import product as iterproduct
 
 import numpy as np
+from scipy.special import loggamma
 
 from .utils import process_header
 
 
 class DataSourceError(IOError):
-    """Data source error.
+    """Raise an exception when the data source is not available or not in
+    the correct format.
 
     """
 
 
 class DataSourceWarning(UserWarning):
-    """Data source warning.
+    """Raise an exception when the data source does not appear to be
+    self-consistent.
 
     """
 
@@ -115,7 +116,7 @@ class LumFuncMeasurements:
 
         if self._uncertainties is not None:
             data_variance = \
-                self._uncertainties.flatten()[self._valid_data_points]
+                self._uncertainties.flatten()[self._valid_data_points] ** 2
         else:
             data_variance = None
 
@@ -145,7 +146,7 @@ class LumFuncMeasurements:
                 z_idx = self.redshift_bins.index(z)
             except (TypeError, ValueError):
                 raise KeyError(
-                    "No measurements for redshift bin '{}'. ".format(z_key)
+                    "No measurements for redshift bin '{}'.".format(z_key)
                 )
 
         if self._uncertainties is not None:
@@ -168,7 +169,7 @@ class LumFuncMeasurements:
 
         if len(mheadings) != len(measurements_array):
             raise DataSourceError(
-                "Number of headings does not match measurements. "
+                "Number of headings does not match measurements."
             )
 
         self.redshift_labels, self.redshift_bins = \
@@ -190,7 +191,7 @@ class LumFuncMeasurements:
 
             brightness_bin_matching_msg = (
                 "Brightness bins in measurements and uncertainties files "
-                "do not match. "
+                "do not match."
             )
             if len(self.brightness_bins) != len(source_uncertainties[0]):
                 raise DataSourceError(brightness_bin_matching_msg)
@@ -202,7 +203,7 @@ class LumFuncMeasurements:
             if np.shape(measurements_array) != np.shape(uncertainties_array):
                 raise DataSourceError(
                     "Uncertainties file data do not match "
-                    "measurements file data. "
+                    "measurements file data."
                 )
 
             with open(self._uncertainties_source_path, 'r') as ufile:
@@ -256,11 +257,11 @@ def _uniform_log_pdf(param_vals, param_ranges):
 
     if len(param_ranges) != len(param_vals):
         raise ValueError(
-            "Number of parameter ranges does not match number of parameters. "
+            "Number of parameter ranges does not match number of parameters."
         )
     if any(np.greater(param_ranges[:, 0], param_ranges[:, 1])):
         param_ranges = np.sort(param_ranges, axis=-1)
-        warnings.warn("Uniform prior ranges reordered. ")
+        warnings.warn("Uniform prior ranges reordered.")
 
     if any(np.less(param_vals, param_ranges[:, 0])) \
             or any(np.greater(param_vals, param_ranges[:, 1])):
@@ -282,6 +283,22 @@ def _normal_log_pdf(data_vector, model_vector, covariance_matrix):
             np.linalg.inv(covariance_matrix),
             data_vector - model_vector
         ]
+    )
+
+    return log_p
+
+
+def _poisson_log_pdf(data_vector, model_vector):
+
+    data_vector = np.asarray(data_vector)
+    model_vector = np.asarray(model_vector)
+
+    if not all(np.isfinite(model_vector)):
+        return - np.inf
+
+    log_p = - np.sum(
+        data_vector * np.log(model_vector)
+        - model_vector - loggamma(data_vector + 1.)
     )
 
     return log_p
@@ -313,13 +330,15 @@ class LumFuncLikelihood(LumFuncMeasurements):
     fixed_file : str or :class:`pathlib.Path` or None, optional
         Luminosity function model fixed parameter file path.  This covers
         any model parameter(s) not included in the prior.
+    model_constraint : callable or None, optional
+        Additional model constraint(s) to be imposed on model parameters
+        as a prior (default is `None`).
+    distribution : {'normal', 'poisson'}, optional
+        Likelihood distribution for the data vector (default is 'normal').
     data_covariance : float array_like or None, optional
         Covariance matrix for the data points.  Its dimensions must match
         the length of the data vector for valid data points ordered by
         brightness and redshift.
-    model_constraint : callable or None, optional
-        Additional model constraint(s) to be imposed on model parameters
-        as a prior (default is `None`).
     base10_log : bool, optional
         If `True` (default), all values are converted to base-10
         logarithms.
@@ -339,7 +358,8 @@ class LumFuncLikelihood(LumFuncMeasurements):
 
     def __init__(self, lumfunc_model, measurements_file, prior_file,
                  uncertainties_file=None, fixed_file=None,
-                 data_covariance=None, model_constraint=None, base10_log=True):
+                 model_constraint=None, distribution='normal',
+                 data_covariance=None, base10_log=True):
 
         super().__init__(
             measurements_file,
@@ -355,6 +375,7 @@ class LumFuncLikelihood(LumFuncMeasurements):
         self._fixed_source_path = fixed_file
         self.prior, self.fixed = self._setup_prior()
 
+        self._distribution = distribution
         self._external_data_covariance = data_covariance
         self._data_vector, self._data_covariance = self._get_moments()
 
@@ -408,9 +429,16 @@ class LumFuncLikelihood(LumFuncMeasurements):
             for data_point in self.data_points
         ]
 
-        log_likelihood = _normal_log_pdf(
-            self._data_vector, model_vector, self._data_covariance
-        )
+        if self._distribution == 'normal':
+            log_likelihood = _normal_log_pdf(
+                self._data_vector, model_vector, self._data_covariance
+            )
+        elif self._distribution == 'poisson':
+            log_likelihood = _poisson_log_pdf(self._data_vector, model_vector)
+        else:
+            raise ValueError(
+                f"Unsupported distribution: {self._distribution}."
+            )
 
         return log_prior + log_likelihood
 
@@ -455,7 +483,7 @@ class LumFuncLikelihood(LumFuncMeasurements):
                     or len(_data_covar) != len(self.data_points):
                 raise ValueError(
                     "`data_covariance` dimensions do not match data points: "
-                    "({:d}, {:d}) versus {:d}. "
+                    "({:d}, {:d}) versus {:d}."
                     .format(*np.shape(_data_covar), len(self.data_points))
                 )
             return _data_mean, _data_covar
@@ -474,5 +502,7 @@ class LumFuncLikelihood(LumFuncMeasurements):
         return (
             "LuminosityFunctionLikelihood"
             "(measurements_source='{}',LF_model='{}')"
-            .format(self._measurements_source_path, self._lumfunc_model.__name__)
+            .format(
+                self._measurements_source_path, self._lumfunc_model.__name__
+            )
         )

horizonground/lumfunc_modeller.py
@@ -128,8 +128,6 @@ This is a parametric model with 6 parameters: :math:`\alpha, \epsilon,
     :trim:
 
 """
-from __future__ import division
-
 import numpy as np
 from astropy import units
 from scipy.integrate import quad
@@ -187,17 +185,17 @@ def quasar_PLE_model(magnitude, redshift, redshift_pivot=2.2, base10_log=True,
 
     # Evaluate the model prediction.
     exponent_magnitude_factor = M_g - M_g_star_p \
-         + 2.5*(k_1 * (z - z_p) + k_2 * (z - z_p)**2)
+         + 2.5 * (k_1 * (z - z_p) + k_2 * (z - z_p) ** 2)
 
     try:
-        faint_power_law = 10 ** (0.4*(alpha + 1) * exponent_magnitude_factor)
+        faint_power_law = 10 ** (0.4 * (alpha + 1) * exponent_magnitude_factor)
     except OverflowError:
         if (alpha + 1) * exponent_magnitude_factor > 0.:
             faint_power_law = np.inf
         else:
             faint_power_law = 0.
     try:
-        bright_power_law = 10 ** (0.4*(beta + 1) * exponent_magnitude_factor)
+        bright_power_law = 10 ** (0.4 * (beta + 1) * exponent_magnitude_factor)
     except OverflowError:
         if (beta + 1) * exponent_magnitude_factor > 0.:
             bright_power_law = np.inf
@@ -209,7 +207,7 @@ def quasar_PLE_model(magnitude, redshift, redshift_pivot=2.2, base10_log=True,
         comoving_density = lg_Phi_star \
             - np.log10(faint_power_law + bright_power_law)
     else:
-        Phi_star = 10**model_parameters[r'\lg\Phi_\ast']
+        Phi_star = 10 ** model_parameters[r'\lg\Phi_\ast']
         comoving_density = Phi_star / (faint_power_law + bright_power_law)
 
     return comoving_density
@@ -332,30 +330,32 @@ def alpha_emitter_schechter_model(lg_luminosity, redshift, base10_log=True,
     if base10_log:
         lg_Phi_star0 = model_parameters[r'\lg\Phi_{\ast0}']
     else:
-        Phi_star0 = 10**model_parameters[r'\lg\Phi_{\ast0}']
+        Phi_star0 = 10 ** model_parameters[r'\lg\Phi_{\ast0}']
 
     if z <= z_b:
         if base10_log:
             lg_Phi_star = lg_Phi_star0 + epsilon * np.log10(1 + z)
         else:
-            Phi_star = Phi_star0 * (1 + z)**epsilon
+            Phi_star = Phi_star0 * (1 + z) ** epsilon
     else:
         if base10_log:
             lg_Phi_star = lg_Phi_star0 \
-                + 2*epsilon * np.log10(1 + z_b) \
+                + 2 * epsilon * np.log10(1 + z_b) \
                 - epsilon * np.log10(1 + z)
         else:
-            Phi_star = Phi_star0 * (1 + z_b)**(2*epsilon) / (1 + z)**epsilon
+            Phi_star = \
+                Phi_star0 * (1 + z_b) ** (2 * epsilon) / (1 + z) ** epsilon
 
     if base10_log:
         lg_y0 = lg_luminosity - model_parameters[r'\lg{L_{\ast0}}']
         lg_y = lg_y0 - delta * np.log10(1 + z)
         comoving_density = np.log10(np.log(10)) + lg_Phi_star \
-            + (alpha + 1) * lg_y - np.log10(np.e) * 10**lg_y
+            + (alpha + 1) * lg_y - np.log10(np.e) * 10 ** lg_y
     else:
-        y0 = 10**(lg_luminosity - model_parameters[r'\lg{L_{\ast0}}'])
-        y = y0 / (1 + z)**delta
-        comoving_density = np.log(10) * Phi_star * y**(alpha + 1) * np.exp(-y)
+        y0 = 10 ** (lg_luminosity - model_parameters[r'\lg{L_{\ast0}}'])
+        y = y0 / (1 + z) ** delta
+        comoving_density = \
+            np.log(10) * Phi_star * y ** (alpha + 1) * np.exp(-y)
 
     return comoving_density
 
@@ -443,8 +443,8 @@ class LumFuncModeller:
         self._threshold_variable = self._alias(threshold_variable)
         if self._threshold_variable == 'flux':
             self.brightness_threshold = lambda z: np.log10(
-                4*np.pi * threshold_value
-                * self.cosmology.luminosity_distance(z).to(units.cm).value**2
+                4 * np.pi * threshold_value
+                * self.cosmology.luminosity_distance(z).to(units.cm).value ** 2
             )
             self._threshold_variable = 'luminosity'
         else:
@@ -454,7 +454,7 @@ class LumFuncModeller:
         if self._threshold_variable != self.brightness_variable:
             raise ValueError(
                 "`threshold_variable` '{}' does not match "
-                "`brightness_variable` '{}'. "
+                "`brightness_variable` '{}'."
                 .format(self._threshold_variable, self.brightness_variable)
             )
 
@@ -529,7 +529,7 @@ class LumFuncModeller:
                 self.brightness_bound[self.brightness_variable],  # bright end
                 args=(redshift,)
             )[0]
-        ) / self.cosmology.h**3
+        ) / self.cosmology.h ** 3
 
         return _comoving_number_density
 
@@ -568,9 +568,9 @@ class LumFuncModeller:
 
         """
         if self.brightness_variable == 'luminosity':
-            prefactor = 2/5 * 1/np.log(10)
+            prefactor = 2./5. /np.log(10)
         elif self.brightness_variable == 'magnitude':
-            prefactor = 1/np.log(10)
+            prefactor = 1 / np.log(10)
 
         _magnification_bias = prefactor * self.luminosity_function(
             self.brightness_threshold(redshift), redshift
@@ -589,8 +589,8 @@ class LumFuncModeller:
             if brightness_variable.lower().startswith('m'):
                 return 'magnitude'
         except AttributeError:
-            raise TypeError("Brightness variable must be a string. ")
+            raise TypeError("Brightness variable must be a string.")
 
         raise ValueError(
-            f"Unrecognised brightness variable: '{brightness_variable}'. "
+            f"Unrecognised brightness variable: '{brightness_variable}'."
         )