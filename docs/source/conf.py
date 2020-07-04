2d40f0d91198c79a7b3394f2d35428006ef8d53c
Mike Shengbo Wang <mike.wang@port.ac.uk>
2020-07-05 00:59:51 +0100

    Updated numpy intersphinx url.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ autosummary_generate = True
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://docs.scipy.org/doc/numpy', None),
+    'numpy': ('https://numpy.org/doc/stable/objects.inv', None),
     'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
     'matplotlib': ('https://matplotlib.org', None),
     'astropy': ('https://docs.astropy.org/en/stable/', None),
